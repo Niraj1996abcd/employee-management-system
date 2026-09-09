e5e6f527512393fdd510ebcd0c1b03f70ecc9fa2
Anmol <Anmol@VRVIRTUAL.COM>
2026-09-09 09:45:14 +0530

    Add employee pagination

--- server/src/controllers/employeeController.ts
+++ server/src/controllers/employeeController.ts
@@ -20,19 +20,37 @@ export const createEmployee = async (
 };
 // GET ALL EMPLOYEES
 export const getEmployees = async (
-  _req: Request,
+  req: Request,
   res: Response,
   next: NextFunction,
 ) => {
   try {
-    const employees = await Employee.find().sort({
-      createdAt: -1,
-    });
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+
+    const skip = (page - 1) * limit;
+
+    const employees = await Employee.find()
+      .sort({
+        createdAt: -1,
+      })
+      .skip(skip)
+      .limit(limit);
+
+    const totalEmployees = await Employee.countDocuments();
+
+    const totalPages = Math.ceil(totalEmployees / limit);
 
     res.status(200).json({
       success: true,
       message: "Employees fetched successfully",
       data: employees,
+      pagination: {
+        page,
+        limit,
+        totalEmployees,
+        totalPages,
+      },
     });
   } catch (error) {
     next(error);
